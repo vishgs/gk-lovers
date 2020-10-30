a3eae5253129fe9644915e4af8deaa4ceebad52d
vishgs <vishnugs2016@gmail.com>
2020-10-30 21:22:28 +0530

    added all files

---
+++ index.js
@@ -0,0 +1,23 @@
+function whatsapp(){
+
+    window.location.href="https://chat.whatsapp.com/CQBH32gFDmPBawxpLVjo6i"
+
+}
+
+function telegram(){
+
+    window.location.href="https://t.me/gklovers"
+
+}
+
+function youtube(){
+
+    window.location.href="https://www.youtube.com/channel/UCIEY3UStz9E_w8mSXVfV5pQ"
+
+}
+
+function blog(){
+
+    window.location.href="https://www.gklovers.com/"
+
+}
