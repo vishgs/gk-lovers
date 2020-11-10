e78168b6ae81e5b9a94172fe37282e47bc27a8c3
vishgs <vishnugs2016@gmail.com>
2020-11-11 02:59:46 +0530

    changed navbar style according to screen size

--- index.js
+++ index.js
@@ -27,3 +27,20 @@ function app() {
     window.location.href = "https://play.google.com/store/apps/details?id=com.dockode.rental.gklovers"
 
 }
+
+function changeColor(){
+
+    window.onresize = function(){
+
+        if(window.innerWidth > 992){
+
+            document.getElementById("navbarSupportedContent").style.background = "transparent";
+
+        }else {
+
+            document.getElementById("navbarSupportedContent").style.background = "white";
+            document.getElementById("navbarSupportedContent").style.padding = "10px";
+        
+        }
+    }
+}
